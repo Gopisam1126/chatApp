f8b99f12533d9a565204b4f78c699da68b4cfee2
scriptKid <gopikrishnan0045@gmail.com>
2024-09-27 21:14:20 +0530

    add

--- FRONTEND/src/assets/components/navbar.jsx
+++ FRONTEND/src/assets/components/navbar.jsx
@@ -1,6 +1,26 @@
+/* eslint-disable no-unused-vars */
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import { Link } from "react-router-dom"
+import "../componentStyles/navbar.css"
 function Navbar() {
     return <>
-        <h1>Navbar</h1>
+        <section className='navbar-main-sec'>
+            <nav className='navbar-container'>
+                <ul className='nav-ul'>
+                    <Link to="/">
+                        <li className='nav-li nav-li-1'>
+                            Home
+                        </li>
+                    </Link>
+                    <Link to="/login">
+                        <li className='nav-li nav-li-1'>
+                            Login
+                        </li>
+                    </Link>
+                </ul>
+            </nav>
+        </section>
     </>
 }
 
