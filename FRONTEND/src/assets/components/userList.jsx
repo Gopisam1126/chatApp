57a7dcf40bff6ca7678c026904b959f2b854c3ea
scriptKid <gopikrishnan0045@gmail.com>
2024-10-05 22:50:37 +0530

    mod charroom

--- FRONTEND/src/assets/components/userList.jsx
+++ FRONTEND/src/assets/components/userList.jsx
@@ -1,8 +1,17 @@
-import "../componentStyles/userlist.css"
+import "../componentStyles/userlist.css";
+import AddIcon from '@mui/icons-material/Add';
 function UserList() {
     return <>
         <section className="userlist-main-sec">
-            <h4>CHATS</h4>
+            <div className="chat-a-icon">
+                <h5>CHATS</h5>
+                <div className="icon-container">
+                    <AddIcon style={{
+                        color: "#16423C"
+                    }}/>
+                </div>
+            </div>
+            <hr className="chat-b-hr" />
         </section>
     </>
 }
