f9038b321054b3766740ad11ff8a2c81821434ac
scriptKid <gopikrishnan0045@gmail.com>
2024-10-07 20:33:10 +0530

    add sepp sroll | mod chatroom

--- FRONTEND/src/assets/components/charRoomHeader.jsx
+++ FRONTEND/src/assets/components/charRoomHeader.jsx
@@ -1,6 +1,56 @@
+import "../componentStyles/chatRoomHeader.css";
+// import { useState } from "react";
+import SearchIcon from '@mui/icons-material/Search';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import VideocamOutlinedIcon from '@mui/icons-material/VideocamOutlined';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
 function CRHeader() {
+
+    // const [isActive, setIsActive] = useState(false);
+
     return <>
-        <h5>CR Header</h5>
+        <section className="chat-header-sec">
+            <div className="profile-header">
+                <div className="profile-det-container">
+                    <img src="\images\p1.jpg" alt="p1" className="pro-img-det" />
+                    <p className="pro-det-name">John Doe</p>
+                    <div className="func-icons">
+                        <SearchIcon className="func-icon func-icon1" style={{
+                            marginLeft: "1.5vw",
+                            color: '#1A3636',
+                            cursor: "pointer",
+                            fontSize: '2rem'
+                        }}/>
+                        <LocalPhoneIcon className="func-icon" style={{
+                            marginLeft: "1.5vw",
+                            color: '#1A3636',
+                            cursor: "pointer",
+                            fontSize: '2rem'
+                        }}/>
+                        <VideocamOutlinedIcon className="func-icon" style={{
+                            marginLeft: "1.5vw",
+                            color: '#1A3636',
+                            cursor: "pointer",
+                            fontSize: '2rem'
+                        }}/>
+                        <InfoOutlinedIcon className="func-icon" style={{
+                            marginLeft: "1.5vw",
+                            color: '#1A3636',
+                            cursor: "pointer",
+                            fontSize: '2rem'
+                        }}/>
+                        <MoreVertOutlinedIcon className="func-icon" style={{
+                            marginLeft: "1.5vw",
+                            color: '#1A3636',
+                            cursor: "pointer",
+                            fontSize: '2rem'
+                        }}/>
+                    </div>
+                </div>
+                {/* <span className="active-status">Active</span> */}
+            </div>
+        </section>
     </>
 }
 
